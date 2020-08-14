d783dc57dceed613dc88b8c920489a21dbfab8db
binbin <3530356178@qq.com>
2020-08-14 20:01:09 +0800

    测试分支

--- TestGit/src/cn/lxb/testdemo/Test01.java
+++ TestGit/src/cn/lxb/testdemo/Test01.java
@@ -1,25 +1,6 @@
 package cn.lxb.testdemo;
 
 public interface Test01 {
-	//第一个Git测试方法
-	public void testGit01();
-	
-	//binbin更新了方法
-	public void testGit02();
-	
-	//binbin又更新了方法
-	public void testGit03();
-	
-	//binbin又添加了方法testGit04
-	public void testGit04();
-	
-	//dandan又添加了方法testGit05
-	public void testGit05();
-
-	//dandan又添加了方法testGit06
-	public void testGit06();
-	
-	//dandan又添加了方法testGit07
-		public void testGit07();
-	
+	//测试分支
+	public void test();
 }
